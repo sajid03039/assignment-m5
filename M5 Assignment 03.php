2b2b33890a8a7607cbac1bded03ef33632d53205
sajid03039 <1351sajid@gmail.com>
2024-07-07 21:27:17 +0600

    main

---
+++ M5 Assignment 03.php
@@ -0,0 +1,9 @@
+<?php
+$celsius = 32;
+
+// Convert Celsius to Fahrenheit
+$fahrenheit = ($celsius * 9/5) + 32;
+
+// To print the result with 2 decimal points
+printf("Temperature in Fahrenheit: %.2f", $fahrenheit);
+?>
